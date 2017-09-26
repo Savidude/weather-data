efb4eba7eb49c6571e9700df752c9bce61551c24
Savidude <mysticsavy@gmail.com>
2017-09-26 08:46:09 +0530

    Changing timezone issues

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -386,90 +386,6 @@ function init_flot_chart(){
             timeformat: "%I:%M %p"
         }
     };
-
-
-    // var chart_plot_02_data = [];
-    //
-    // for (var i = 0; i < 30; i++) {
-    //     chart_plot_02_data.push([new Date(Date.today().add(i).days()).getTime(), randNum() + i + i + 10]);
-    // }
-    //
-    // var chart_plot_02_settings = {
-    //     grid: {
-    //         show: true,
-    //         aboveData: true,
-    //         color: "#3f3f3f",
-    //         labelMargin: 10,
-    //         axisMargin: 0,
-    //         borderWidth: 0,
-    //         borderColor: null,
-    //         minBorderMargin: 5,
-    //         clickable: true,
-    //         hoverable: true,
-    //         autoHighlight: true,
-    //         mouseActiveRadius: 100
-    //     },
-    //     series: {
-    //         lines: {
-    //             show: true,
-    //             fill: true,
-    //             lineWidth: 2,
-    //             steps: false
-    //         },
-    //         points: {
-    //             show: true,
-    //             radius: 4.5,
-    //             symbol: "circle",
-    //             lineWidth: 3.0
-    //         }
-    //     },
-    //     legend: {
-    //         position: "ne",
-    //         margin: [0, -25],
-    //         noColumns: 0,
-    //         labelBoxBorderColor: null,
-    //         labelFormatter: function(label, series) {
-    //             return label + '&nbsp;&nbsp;';
-    //         },
-    //         width: 40,
-    //         height: 1
-    //     },
-    //     colors: ['#96CA59', '#3F97EB', '#72c380', '#6f7a8a', '#f7cb38', '#5a8022', '#2c7282'],
-    //     shadowSize: 0,
-    //     tooltip: true,
-    //     tooltipOpts: {
-    //         content: "%s: %y.0",
-    //         xDateFormat: "%d/%m",
-    //         shifts: {
-    //             x: -30,
-    //             y: -50
-    //         },
-    //         defaultTheme: false
-    //     },
-    //     yaxis: {
-    //         min: 0
-    //     },
-    //     xaxis: {
-    //         mode: "time",
-    //         timeformat: "%y/%m/%d"
-    //     }
-    // };
-    //
-    // if ($("#chart_plot_02").length){
-    //     console.log(JSON.stringify(chart_plot_02_data, null, 2));
-    //     $.plot( $("#chart_plot_02"),
-    //         [{
-    //             label: "Something",
-    //             data: chart_plot_02_data,
-    //             lines: {
-    //                 fillColor: "rgba(150, 202, 89, 0.12)"
-    //             },
-    //             points: {
-    //                 fillColor: "#fff" }
-    //         }], chart_plot_02_settings);
-    //
-    // }
-
 }
 
 /* AUTOSIZE */
@@ -761,18 +677,24 @@ function plotWeatherData(weatherData) {
     humidityPlotData = [];
     windspdPlotData = [];
     winddirPlotData = [];
+
+    var offsetTime = new Date().getTimezoneOffset();
+    var offsetTimeMills = ((offsetTime) * 60) * 1000 * (-1);
+
     weatherData.forEach (function (data) {
-        rainfallPlotData.push([data.recDateTime, data.rainfall]);
-        tempPlotData.push([data.recDateTime, data.temp]);
-        humidityPlotData.push([data.recDateTime, data.humidity]);
-        windspdPlotData.push([data.recDateTime, data.windspd]);
-        winddirPlotData.push([data.recDateTime, data.winddir]);
+        var recDateTime = data.recDateTime + offsetTimeMills;
+
+        rainfallPlotData.push([recDateTime, data.rainfall]);
+        tempPlotData.push([recDateTime, data.temp]);
+        humidityPlotData.push([recDateTime, data.humidity]);
+        windspdPlotData.push([recDateTime, data.windspd]);
+        winddirPlotData.push([recDateTime, data.winddir]);
     });
 
-    if (weatherData.length > 0) {
-        plotSettings.xaxis.min = weatherData[0].recDateTime;
-        plotSettings.xaxis.max = weatherData[weatherData.length - 1].recDateTime;
-    }
+    // if (weatherData.length > 0) {
+    //     plotSettings.xaxis.min = weatherData[0].recDateTime + offsetTimeMills;
+    //     plotSettings.xaxis.max = weatherData[weatherData.length - 1].recDateTime + offsetTimeMills;
+    // }
 
     window.location.hash = '#data-plot';
 
